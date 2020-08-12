8fc5e1369f1a2d64615e6de7026e9f246dececa7
Carlos Vallejo <cvallejo.ec@gmail.com>
2020-08-11 19:52:13 -0500

    Multiple changes
    1) Favicon
    2) Hero background
    3) About Me image
    3) Main logo

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
-import Retrato from '../assets/static/retrato.jpeg';
+import Retrato from '../assets/static/retrato-degradado-transparente.png';
 
 const AboutMe = () => {
   useEffect(() => {
